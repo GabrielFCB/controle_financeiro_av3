6ea78de248cdcd750a27b0a7a81791ef618dcba4
Gabriel <gabrielfcb13@gmail.com>
2025-06-08 18:26:39 -0300

    Atualizar user_json.ex para ter tratamento de erro

--- lib/controle_financeiro_av3_web/controllers/json/user_json.ex
+++ lib/controle_financeiro_av3_web/controllers/json/user_json.ex
@@ -16,4 +16,16 @@ defmodule ControleFinanceiroAv3Web.UserJSON do
       data_atualizacao: user.data_atualizacao
     }
   end
+
+  def errors(%{changeset: changeset}) do
+    %{errors: translate_errors(changeset)}
+  end
+
+  defp translate_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
 end
