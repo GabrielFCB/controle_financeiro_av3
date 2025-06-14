209d227ed011b80830cef7f983c2bedb78776958
Gabriel <gabrielfcb13@gmail.com>
2025-06-14 16:55:14 -0300

    Implementar alteacoes de jwt no tag_controller

--- lib/controle_financeiro_av3_web/controllers/tag_controller.ex
+++ lib/controle_financeiro_av3_web/controllers/tag_controller.ex
@@ -2,16 +2,26 @@ defmodule ControleFinanceiroAv3Web.TagController do
   use ControleFinanceiroAv3Web, :controller
   alias ControleFinanceiroAv3.Repo
   alias ControleFinanceiroAv3.Tag
+  import Ecto.Query
 
   # GET /api/tags
   def index(conn, _params) do
-    tags = Repo.all(Tag)
+    current_user_id = conn.assigns.current_user_id
+
+    tags =
+      Tag
+      |> where([t], t.user_id == ^current_user_id)
+      |> Repo.all()
+
     render(conn, "index.json", tags: tags)
   end
 
   # POST /api/tags
   def create(conn, %{"tag" => tag_params}) do
-    changeset = Tag.changeset(%Tag{}, tag_params)
+    current_user_id = conn.assigns.current_user_id
+    params_with_user = Map.put(tag_params, "user_id", current_user_id)
+
+    changeset = Tag.changeset(%Tag{}, params_with_user)
 
     case Repo.insert(changeset) do
       {:ok, tag} ->
@@ -28,38 +38,64 @@ defmodule ControleFinanceiroAv3Web.TagController do
 
   # GET /api/tags/:id
   def show(conn, %{"id" => id}) do
-    tag = Repo.get!(Tag, id)
-    render(conn, "show.json", tag: tag)
+    current_user_id = conn.assigns.current_user_id
+
+    case Repo.get_by(Tag, id: id, user_id: current_user_id) do
+      nil ->
+        conn
+        |> put_status(:not_found)
+        |> json(%{error: "Tag not found"})
+
+      tag ->
+        render(conn, "show.json", tag: tag)
+    end
   end
 
   # PUT /api/tags/:id
   def update(conn, %{"id" => id, "tag" => tag_params}) do
-    tag = Repo.get!(Tag, id)
-    changeset = Tag.changeset(tag, tag_params)
+    current_user_id = conn.assigns.current_user_id
 
-    case Repo.update(changeset) do
-      {:ok, tag} ->
-        render(conn, "show.json", tag: tag)
-
-      {:error, changeset} ->
+    case Repo.get_by(Tag, id: id, user_id: current_user_id) do
+      nil ->
         conn
-        |> put_status(:unprocessable_entity)
-        |> render("errors.json", changeset: changeset)
+        |> put_status(:not_found)
+        |> json(%{error: "Tag not found"})
+
+      tag ->
+        changeset = Tag.changeset(tag, tag_params)
+
+        case Repo.update(changeset) do
+          {:ok, tag} ->
+            render(conn, "show.json", tag: tag)
+
+          {:error, changeset} ->
+            conn
+            |> put_status(:unprocessable_entity)
+            |> render("errors.json", changeset: changeset)
+        end
     end
   end
 
   # DELETE /api/tags/:id
   def delete(conn, %{"id" => id}) do
-    tag = Repo.get!(Tag, id)
+    current_user_id = conn.assigns.current_user_id
 
-    case Repo.delete(tag) do
-      {:ok, _} ->
-        send_resp(conn, :no_content, "")
-
-      {:error, changeset} ->
+    case Repo.get_by(Tag, id: id, user_id: current_user_id) do
+      nil ->
         conn
-        |> put_status(:unprocessable_entity)
-        |> render("errors.json", changeset: changeset)
+        |> put_status(:not_found)
+        |> json(%{error: "Tag not found"})
+
+      tag ->
+        case Repo.delete(tag) do
+          {:ok, _} ->
+            send_resp(conn, :no_content, "")
+
+          {:error, changeset} ->
+            conn
+            |> put_status(:unprocessable_entity)
+            |> render("errors.json", changeset: changeset)
+        end
     end
   end
 end
